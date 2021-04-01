9e295453fb2b5bda22814f2f0a154a34d84b0a2a
dominicmeddick <dominicmeddick@gmail.com>
2021-04-01 12:49:53 +0100

    Step 11 changes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require "docking_station"
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike } #release_bike needs to be symbol  
+  it { is_expected.to respond_to :release_bike } 
 
   it 'get bike and check if bike is working' do
     bike = subject.release_bike
